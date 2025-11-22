fe59aff6bc2eeebab91a97cf9c3122c05542e40f
Aleksis <aleksisdz6@gmail.com>
2025-11-22 20:23:20 +0200

    added Jump and physics || added fog to model

--- game.py
+++ game.py
@@ -1,24 +1,78 @@
-﻿import viz, vizcam, vizfx
+﻿import viz, vizcam, vizfx, vizact
 
 viz.setMultiSample(16)
 viz.fov(90)
 viz.go(viz.FULLSCREEN)
+viz.mouse.setVisible(False)
 
 warehouse = viz.addChild('objects/warehouse.osgb')
-warehouse.enable(viz.LIGHTING)
-
-
+warehouse.collideMesh()
 viz.MainView.getHeadLight().disable()
+vizfx.addPointLight(pos=[0, 18, 0])
 
-#ceiling_light = vizfx.addPointLight(pos=[0, 18, 0])
-#ceiling_light.intensity(2)
+steve = viz.addAvatar('vcc_male.cfg')
+steve.setPosition([20, 2.5, 0])
 
 navigator = vizcam.WalkNavigate(moveScale=2)
 viz.cam.setHandler(navigator)
+viz.MainView.setPosition(15, 2.5, 0)
+viz.MainView.collision()
 
+status_text = viz.addText('', viz.SCREEN)
+status_text.setPosition([0.05, 0.95, 0])
+status_text.color(viz.WHITE)
+status_text.fontSize(20)
 
-viz.MainView.setPosition(0, 4.5, 0)
-viz.MainView.collision(viz.ON)
+crosshair = viz.addText('+', viz.SCREEN)
+crosshair.setPosition([0.5, 0.5, 0])
+crosshair.color(viz.GREEN)
+crosshair.alignment(viz.ALIGN_CENTER)
+crosshair.fontSize(30)
 
-viz.mouse.setVisible(False)
+PLAYER_HEIGHT = 1.82
+JUMP_VELOCITY = 7.0
+GRAVITY = 9.8
+GROUND_CHECK_DIST = 0.5
+y_velocity = 0.0
 
+def get_ground_height():
+	pos = viz.MainView.getPosition()
+	feet_y = pos[1] - PLAYER_HEIGHT
+	info = viz.intersect([pos[0], feet_y + 0.1, pos[2]], [pos[0], feet_y - GROUND_CHECK_DIST, pos[2]])
+	return info.point[1] + PLAYER_HEIGHT if info.valid else None
+
+def jump():
+	global y_velocity
+	if y_velocity == 0:
+		y_velocity = JUMP_VELOCITY
+
+def apply_gravity(dt):
+	global y_velocity
+	y_velocity -= GRAVITY * dt
+
+def update():
+	global y_velocity
+	
+	pos = viz.MainView.getPosition()
+	ground_height = get_ground_height()
+	on_ground = ground_height is not None and abs(pos[1] - ground_height) < 0.1
+	
+	if viz.key.isDown(' ') and on_ground:
+		jump()
+	
+	if y_velocity != 0 or ground_height is None:
+		dt = viz.getFrameElapsed()
+		apply_gravity(dt)
+		new_y = pos[1] + y_velocity * dt
+		
+		if ground_height and new_y <= ground_height and y_velocity < 0:
+			new_y = ground_height
+			y_velocity = 0.0
+		
+		viz.MainView.setPosition([pos[0], new_y, pos[2]])
+	else:
+		viz.MainView.setPosition([pos[0], ground_height, pos[2]])
+	
+	status_text.message(f"Y: {pos[1]:.2f} | Ground: {on_ground} | Vel: {y_velocity:.2f}")
+
+vizact.ontimer(0, update)
