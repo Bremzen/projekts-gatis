465d0d30f3551b131cbec44e348a6e298ec61fd1
Aleksis <aleksisdz6@gmail.com>
2025-11-22 17:49:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+﻿SPĒLES IDEJA:
+
+1 pret 1 snaiperu spēle.
+Ir karte, kur ir 2 spawn zonas katram spēlētājam.
+Spēlētājam ir zemē nomests snaiperis, kurš jāsavāc, lai varētu šaut.
+Uzvar tas, kurš nošauj otru spēlētāju
---
+++ game.py
@@ -0,0 +1,24 @@
+﻿import viz, vizcam, vizfx
+
+viz.setMultiSample(16)
+viz.fov(90)
+viz.go(viz.FULLSCREEN)
+
+warehouse = viz.addChild('objects/warehouse.osgb')
+warehouse.enable(viz.LIGHTING)
+
+
+viz.MainView.getHeadLight().disable()
+
+#ceiling_light = vizfx.addPointLight(pos=[0, 18, 0])
+#ceiling_light.intensity(2)
+
+navigator = vizcam.WalkNavigate(moveScale=2)
+viz.cam.setHandler(navigator)
+
+
+viz.MainView.setPosition(0, 4.5, 0)
+viz.MainView.collision(viz.ON)
+
+viz.mouse.setVisible(False)
+
